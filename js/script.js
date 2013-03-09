7c8a367e9b5906cdc05bcbad8a3c4f54ec6b10d2
Mark Durrant <mark@pebblecode.com>
2013-03-09 15:53:13 +0000

    add ajax & json shiz

--- js/script.js
+++ js/script.js
@@ -1,2 +1,14 @@
 // fit header to width
-jQuery(".title").fitText(.67);
+$(".title").fitText(.67);
+
+$.getJSON('../data/data.json',function(data){
+  $('.f-is-list').empty();
+  $.each(data, function(entryIndex, entry){
+    var html = '<li class="f-is-item">';            
+    html += '<p class="f-is">' + entry['copy'] + '</p>';
+    html += '<p class="name">' + entry['name'] + '</p>';
+    html += '<a href="' + entry['linkUrl'] +'" class="linky">' + entry['linkText'] + '</a>';
+    $('.f-is-list').append(html);
+  });              
+});
+
