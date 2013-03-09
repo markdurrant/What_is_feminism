22d54121eab5018e0ee6cf0595f9017189e951e0
Mark Durrant <mark@pebblecode.com>
2013-03-09 16:49:17 +0000

    get random answer working

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 // fit header to width
 $(".title").fitText(.67);
 
-$.getJSON('../data/data.json',function(data){
-  $('.f-is-list').empty();
-  $.each(data, function(entryIndex, entry){
-    var html = '<li class="f-is-item">';            
-    html += '<p class="f-is">' + entry['copy'] + '</p>';
-    html += '<p class="name">' + entry['name'] + '</p>';
-    html += '<a href="' + entry['linkUrl'] +'" class="linky">' + entry['linkText'] + '</a>';
-    $('.f-is-list').append(html);
-  });              
-});
+$(document).ready(print);
 
+$(".more").click(print);
+
+function print(){
+  $.getJSON('../data/data.json',function(data){
+    var rand = Math.floor(Math.random()*data.length);
+
+    $('.f-is-list').empty()
+    var html = '<li class="f-is-item">';            
+    html += '<p class="f-is">' + data[rand].copy + '</p>';
+    html += '<p class="name">' + data[rand].name + '</p>';
+    html += '<a href="' + data[rand].linkUrl +'" class="linky">' + data[rand].linkText + '</a>';
+    $('.f-is-list').append(html);
+  });
+};
